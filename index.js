7ebc3e7ce0e33f4f5a20412e52805d459840ac4e
Shrithik Raj <lio.srk10@gmail.com>
2025-01-18 23:47:08 +0530

    This is the first commit for the project

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/route1', (req, res)=>{
+    res.send('this is a new server on route 1')
+})
+
+app.get('/route2', (req, res)=>{
+    res.send(`
+        <html>
+        <h1>this is a webpage</h1>
+        </>`)
+       })
+
+app.listen(port, ()=>{
+    console.log(`Express-backend-app listening on port ${port}`)
+})
+
