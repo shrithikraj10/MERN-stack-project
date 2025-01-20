ab37198433391729b85f6c12d186e5dae6f9c4c3
Shrithik Raj <lio.srk10@gmail.com>
2025-01-20 21:27:09 +0530

    Working on adding some functionality to the http server

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/route1', (req, res)=>{
+app.post('/login', (req, res)=>{
     res.send('this is a new server on route 1')
 })
 
-app.get('/route2', (req, res)=>{
+app.post('/signup', (req, res)=>{
     res.send(`
-        <html>
+        html>
         <h1>this is a webpage</h1>
         </>`)
        })
+
+app.get('/Questions', (req, res)=>{
+res.send(`
+    html>
+    <h1>this is a webpage</h1>
+    </>`)
+    })
 
 app.listen(port, ()=>{
     console.log(`Express-backend-app listening on port ${port}`)
